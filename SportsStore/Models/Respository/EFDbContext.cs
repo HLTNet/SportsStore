b2210151973a53ebe83c9a73dd365fe2c5447a9f
hiltan <hiltan@126.com>
2016-09-29 22:21:57 +0800

    初始化

---
+++ SportsStore/Models/Respository/EFDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.Models.Respository
+{
+    public class EFDbContext:DbContext
+    {
+        public DbSet<Product> Proudcts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
