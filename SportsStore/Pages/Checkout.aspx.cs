4906d4862351b995edfad86971229d57cda03046
hiltan <hiltan@126.com>
2016-09-29 22:52:25 +0800

    提交订单

---
+++ SportsStore/Pages/Checkout.aspx.cs
@@ -0,0 +1,48 @@
+﻿using SportsStore.Models;
+using SportsStore.Models.Respository;
+using SportsStore.Pages.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SportsStore.Pages
+{
+    public partial class Checkout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            checkoutForm.Visible = true;
+            checkoutMessage.Visible = false;
+            if(IsPostBack)
+            {
+                Order order = new Order();
+                if (TryUpdateModel(order, new FormValueProvider(ModelBindingExecutionContext)))
+                {
+                    order.OrderLines = new List<OrderLine>();
+
+                    Cart myCart = SessionHelper.GetCart(Session);
+                    foreach(CartLine line in myCart.Lines)
+                    {
+                        order.OrderLines.Add(new OrderLine
+                        {
+                            Order = order,
+                            Product = line.Product,
+                            Quantity = line.Quantity
+                        });
+                    }
+
+                    new Repository().SaveOrder(order);
+                    myCart.Clear();
+
+                    checkoutForm.Visible = false;
+                    checkoutMessage.Visible = true;
+                }
+            }
+
+        }
+    }
+}
